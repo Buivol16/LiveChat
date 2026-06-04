f06a75aa3c70a94f755e3dc03e9cb8349738906a
Denys Khmara <khmaradenis64@gmail.com>
2026-06-04 21:24:48 +0200

    create changelog for private chats and fix old version making default timestamp value with utc timezone.

--- database-schema-validator/src/main/resources/db/changelog/livechat/db.changelog-1.2.sql
+++ database-schema-validator/src/main/resources/db/changelog/livechat/db.changelog-1.2.sql
@@ -1,5 +1,5 @@
 start transaction;
 CREATE EXTENSION IF NOT EXISTS pg_cron;
-CREATE TABLE IF NOT EXISTS "private"."notifications"(uuid varchar(36) PRIMARY KEY , entity_id bigint NOT NULL, type varchar(255) NOT NULL, created_at Timestamp NOT NULL DEFAULT current_timestamp, recently_sent_at Timestamp, status varchar(255) NOT NULL);
+CREATE TABLE IF NOT EXISTS "private"."notifications"(uuid varchar(36) PRIMARY KEY , entity_id bigint NOT NULL, type varchar(255) NOT NULL, created_at Timestamp NOT NULL DEFAULT current_timestamp AT TIME ZONE 'UTC', recently_sent_at Timestamp, status varchar(255) NOT NULL);
 SELECT cron.schedule('change-notification-status-after-30minutes-to-unsent','*/30 * * * *',$$UPDATE notifications SET status = 'UNACCEPTED' WHERE recently_sent_at > current_timestamp AT TIME ZONE 'UTC' - INTERVAL '30 minute' $$);
 commit;
