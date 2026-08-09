edbd61d911fc2ae4b2ace99d8b6c5f168399aca0
Denys Khmara <khmaradenis64@gmail.com>
2026-08-09 15:26:02 +0200

    add transaction to db-changelog

--- database-schema-validator/src/main/resources/db/changelog/livechat/db.changelog-1.4.sql
+++ database-schema-validator/src/main/resources/db/changelog/livechat/db.changelog-1.4.sql
@@ -1,2 +1,4 @@
+start transaction;
 ALTER TABLE "private"."messages" ADD COLUMN is_read boolean not null default false;
 ALTER TABLE private.messages ALTER COLUMN receiver_id DROP NOT NULL;
+commit;
